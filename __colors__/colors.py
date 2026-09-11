092f8ff5f6132012ae66ac549f9de54e0841fd3f
so9810102 <so9810102@gmail.com>
2026-09-11 23:46:44 +0600

    Fix: Update color codes to correct ANSI codes (fc, fb, fm were duplicated)

--- __colors__/colors.py
+++ __colors__/colors.py
@@ -1,17 +1,18 @@
 """
 Color codes for terminal output
 Provides ANSI color codes for colored terminal text
+Updated: September 2026
 """
 
-# ANSI Color Codes
-fc = '\033[94m'   # Foreground Cyan
-fg = '\033[92m'   # Foreground Green
-fr = '\033[91m'   # Foreground Red
-fy = '\033[93m'   # Foreground Yellow
-fb = '\033[94m'   # Foreground Blue
-fm = '\033[95m'   # Foreground Magenta
-sd = '\033[39m'   # Standard/Default
-sb = '\033[39m'   # Standard/Default Bold
+# ANSI Color Codes - FIXED VERSION
+fc = '\033[36m'   # Foreground Cyan
+fg = '\033[32m'   # Foreground Green
+fr = '\033[31m'   # Foreground Red
+fy = '\033[33m'   # Foreground Yellow
+fb = '\033[34m'   # Foreground Blue
+fm = '\033[35m'   # Foreground Magenta
+sd = '\033[0m'    # Reset/Default
+sb = '\033[1m'    # Bold
 
 # Alternative names
 CYAN = fc
@@ -21,3 +22,7 @@ YELLOW = fy
 BLUE = fb
 MAGENTA = fm
 DEFAULT = sd
+BOLD = sb
+
+# Reset code for terminal
+RESET = '\033[0m'
